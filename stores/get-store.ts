fdd85fc37bb3d665a8c90075afeef1abf49f56f7
hellomraza <hellomraza@gmail.com>
2026-05-19 13:35:08 +0530

    refactor(gst-store): simplify state management by removing actions wrapper

--- stores/get-store.ts
+++ stores/get-store.ts
@@ -1,18 +1,29 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { useShallow } from "zustand/shallow";
 
 type GstStore = {
   gstEnabled: boolean;
-  actions: {
-    toggleGst: () => void;
-  };
+  toggleGst: () => void;
 };
-const useGstStore = create<GstStore>((set) => ({
-  gstEnabled: true,
-  actions: {
-    toggleGst: () => set((state) => ({ gstEnabled: !state.gstEnabled })),
-  },
-}));
+const useGstStore = create<GstStore>()(
+  persist(
+    (set) => ({
+      gstEnabled: true,
+      toggleGst: () => set((state) => ({ gstEnabled: !state.gstEnabled })),
+    }),
+    {
+      name: "gst-storage",
+      partialize: (state) => ({ gstEnabled: state.gstEnabled }),
+    },
+  ),
+);
 
 export const useIsGstEnabled = () => useGstStore((state) => state.gstEnabled);
 
-export const useGstActions = () => useGstStore((state) => state.actions);
+export const useGstActions = () =>
+  useGstStore(
+    useShallow((state) => {
+      return { toggleGst: state.toggleGst };
+    }),
+  );
